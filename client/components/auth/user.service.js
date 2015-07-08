790139b7d9373f28b78fe8e870905ce2d267da50
Jianzheng Wu <jianzheng.wu@gmail.com>
2015-07-08 23:34:31 +0200

    tiny

--- client/components/auth/user.service.js
+++ client/components/auth/user.service.js
@@ -1,18 +1,24 @@
 'use strict';
-
+//custom service "User" has two methods, get the current user and change the password of the current user
 angular.module('freightinsightApp')
   .factory('User', function ($resource) {
+//    get the ressource from the server api/user/index.js. the parameter id is me and the parameter controller is the password
+    
     return $resource('/api/users/:id/:controller', {
       id: '@_id'
     },
     {
-      changePassword: {
+
+ //router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
+        changePassword: {
         method: 'PUT',
         params: {
           controller:'password'
         }
       },
-      get: {
+
+//index.js: router.get('/me', auth.isAuthenticated(), controller.me);
+        get: {
         method: 'GET',
         params: {
           id:'me'
